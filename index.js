dd21f3f28bedfd215a88d59c326319cc417e8a7e
MikeMike39 <Mfonseca294@gmail.com>
2021-03-19 17:00:55 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+
+// // Make sure you remove the <main> with id 'main': expected <main id="main"></main> to not exist
+const main = document.getElementById("main");
+main.remove()
+// has a 'newHeader' variable that points to node 'h1#victory
+const newHeader = document.createElement(`h1`);
+/// has a 'newHeader' variable that points to node 'h1#victory'
+newHeader.id ="victory"
+// // has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
+newHeader.innerHTML = "Mike is the champion"
